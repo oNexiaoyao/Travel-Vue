27d3a5bb7baf58524a579dc9c2c8fb8456fb2e65
oNexiaoyao <931925213@qq.com>
2019-09-01 15:44:06 +0800

    代码提交

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">HelloWorld</router-link>|
       <router-link to="/Home">Home</router-link>
-    </div>
+    </div> -->
     <router-view />
   </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router/index';
 // 解决移动端 300ms 点击延迟
 import fastClick from 'fastclick';
 // 重置浏览器样式
-import 'normalize.css';
+// import 'normalize.css';
+import './assets/styles/reset.css';
 // 解决移动端 1px 边框问题
-import './assets/border.css';
-
+import './assets/styles/border.css';
+// 引入图标
+import './assets/styles/iconfont.css';
 
 Vue.config.productionTip = false;
 if ('addEventListener' in document) {
